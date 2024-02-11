e0862444433f161e28c602e5c2e4f70ccdce09b0
Aryan-219 <aryanagr219@gmail.com>
2024-02-11 20:04:20 +0530

    D10: Profile.jsp & proico.js

---
+++ static/js/proico.js
@@ -0,0 +1,9 @@
+<script>
+  const proico = document.querySelector("#proico");
+  proico.addEventListener("mouseover", () => {
+    proico.classList.add("fa-beat");
+  });
+  proico.addEventListener("mouseout", () => {
+    proico.classList.remove("fa-beat");
+  });
+</script>
